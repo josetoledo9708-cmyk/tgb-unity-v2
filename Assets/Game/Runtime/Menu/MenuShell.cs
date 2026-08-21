588e0ee575076da9753c478db12eab76ba30db98
Rinco <josetoledo9708@gmail.com>
2026-08-21 13:20:56 -0600

    tweak(menu): fade de transicion a 3.45s

--- Assets/Game/Runtime/Menu/MenuShell.cs
+++ Assets/Game/Runtime/Menu/MenuShell.cs
@@ -137,7 +137,7 @@ namespace Game.Runtime.Menu
             // cortina de transición (cubre el instante del cambio y da tiempo a cargar el fondo)
             EnsureFade();
             _fader.transform.SetAsLastSibling();
-            _fader.Play(0.45f);
+            _fader.Play(3.45f);
         }
 
         private void EnsureFade()
